79e89189858961ea97fb7c83dd5bdab20c61da3e
taem <taem@taemui-MacBookAir.local>
2025-08-28 13:25:54 +0900

    testFS

--- hello/fs.js
+++ hello/fs.js
@@ -2,6 +2,7 @@ import fs from "fs";
 
 function testFS() {
   console.log("fs START");
+
   fs.writeFileSync("test.txt", "Hello World");
   const data = fs.readFileSync("test.txt", "utf8");
   console.log("test.txt:", data);
